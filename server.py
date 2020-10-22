b78083fa5333b9cea8f8f3b836b31ed3286623be
amolgautam25 <amol_gautam25@yahoo.co.in>
2020-10-21 22:49:30 -0700

    Basic socket with multi threading

--- server.py
+++ server.py
@@ -4,22 +4,31 @@ import socket
 import _thread
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 7734        # Port to listen on (non-privileged ports are > 1023)
+PORT = 7734  # Port to listen on (non-privileged ports are > 1023)
 
-#Bind the port to the given port number
+# Bind the port to the given port number
 ssocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ssocket.bind((HOST,PORT))
+ssocket.bind((HOST, PORT))
 ssocket.listen(5)
 
-print ("Server is Up")
+print("Server is Up")
 
-list_of_peers = {}      #A dictionary whose key is the RFC number and value is the list of peers using the RFC
-rfc_list = {}			#A dictionary whose key is the RFC number and value is the title
-current_peers = {} 		#A dictionary which contains active peers with the key representing the hostname and the value representing the port used for connecting to the server
+list_of_peers = {}
+rfc_list = {}
+current_peers = {}
 
-def connection_handler (connectionsocket,addr):
-    data=connectionsocket.recv(1024)
-    print("The data sent was ( in bytes) "+ str(data))
+
+def connection_handler(connectionsocket, addr):
+    while 1:
+        data = connectionsocket.recv(1024)
+        print("The data sent was ( in bytes) " + str(data))
+        #process the data
+
+        #if the client needs to send more data .
+
+        if not data:
+            break
+
 
 while 1:
     connectionsocket, addr = ssocket.accept()
